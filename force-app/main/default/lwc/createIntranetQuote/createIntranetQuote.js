a461b8109a6485236b380daf8f42e1981be49bb8
francesco.colì <francesco.coli@nexusat.it>
2022-09-14 15:07:41 +0200

    Fix js File not uploaded

--- force-app/main/default/lwc/createIntranetQuote/createIntranetQuote.js
+++ force-app/main/default/lwc/createIntranetQuote/createIntranetQuote.js
@@ -5,6 +5,7 @@ export default class FileUploaderCompLwc extends LightningElement {
     @api recordId;
     fileData
     @track showLoading=false;
+    fileUploaded=false;
     openfileUpload(event) {
         const file = event.target.files[0]
         var reader = new FileReader()
@@ -18,29 +19,41 @@ export default class FileUploaderCompLwc extends LightningElement {
             console.log(this.fileData)
         }
         reader.readAsDataURL(file)
+        if (reader.result) {
+            this.fileUploaded=true;
+        }
     }
     
     handleClick(){
-        this.showLoading=true
-        const {base64, filename, recordId} = this.fileData
-        
-            uploadFile({ base64, filename, recordId }).then(result=>{
-                this.fileData = null
-                this.showLoading=false
-                let title = `${filename} uploaded successfully!!`
-                this.toast(title)
-                eval("$A.get('e.force:refreshView').fire();");
-            }).catch(error=>{   
-                                const title = 'Upload failed because field PaymentType__c is empty'
-                                const toastEvent1 = new ShowToastEvent({
-                                title, 
-                                variant:"error"
+        if (this.fileUploaded) {
+            this.showLoading=true
+            const {base64, filename, recordId} = this.fileData
+            
+                uploadFile({ base64, filename, recordId }).then(result=>{
+                    this.fileData = null
+                    this.showLoading=false
+                    let title = `${filename} uploaded successfully!!`
+                    this.toast(title)
+                    eval("$A.get('e.force:refreshView').fire();");
+                }).catch(error=>{   
+                                    const title = 'Upload failed because field PaymentType__c is empty'
+                                    const toastEvent1 = new ShowToastEvent({
+                                    title, 
+                                    variant:"error"
+                                    })
+                                    console.log(title)
+                                    this.dispatchEvent(toastEvent1)  
+                                    this.showLoading=false              
                                 })
-                                console.log(title)
-                                this.dispatchEvent(toastEvent1)  
-                                this.showLoading=false              
-                             })
-        
+        }
+        else {
+            const title = 'File not uploaded'
+            const toastEvent2 = new ShowToastEvent({
+            title, 
+            variant:"error"
+        })
+        this.dispatchEvent(toastEvent2)
+        }   
        }
     toast(title){
         const toastEvent = new ShowToastEvent({
