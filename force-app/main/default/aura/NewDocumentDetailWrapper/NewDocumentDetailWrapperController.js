214ff46af4d64de46de8ff3831db8e8a9a53a204
Alessio Marra <alessio.marra@nexusat.it>
2023-01-20 18:17:48 +0100

    feat(LNKSSFDC-376): rework new action override for DocumentDetailAyante

--- force-app/main/default/aura/NewDocumentDetailWrapper/NewDocumentDetailWrapperController.js
+++ force-app/main/default/aura/NewDocumentDetailWrapper/NewDocumentDetailWrapperController.js
@@ -1,7 +1,17 @@
+/**
+ * @description       : 
+ * @author            : mattia.miglietta@nexusat.it
+ * @last modified on  : 20/01/2023
+ * @last modified by  : ¤ → alessio.marra@nexusat.it
+**/
 ({
     init : function(cmp, event, helper) {
-        var pageRef = cmp.get("v.pageReference");
-        var state = pageRef.state; // state holds any query params
+        const pageReference = cmp.get("v.pageReference");
+        console.log(JSON.parse(JSON.stringify(pageReference)));
+
+        cmp.set("v.recordTypeId", pageReference.state.recordTypeId);
+
+        var state = pageReference.state; // state holds any query params
         var base64Context = state.inContextOfRef;
 
         // For some reason, the string starts with "1.", if somebody knows why,
@@ -10,6 +20,7 @@
             base64Context = base64Context.substring(2);
         }
         var addressableContext = JSON.parse(window.atob(base64Context));
+        console.log(JSON.parse(JSON.stringify(addressableContext.attributes)));
         cmp.set("v.recordId", addressableContext.attributes.recordId);
     }
 })
--- force-app/main/default/lwc/createDocumentDetailAlyante/createDocumentDetailAlyante.js
+++ force-app/main/default/lwc/createDocumentDetailAlyante/createDocumentDetailAlyante.js
@@ -1,7 +1,7 @@
 /**
  * @description       : 
  * @author            : 
- * @last modified on  : 19/12/2022
+ * @last modified on  : 20/01/2023
  * @last modified by  : ¤ → alessio.marra@nexusat.it
 **/
 import { api, LightningElement, track, wire } from 'lwc';
@@ -10,18 +10,36 @@ import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import getRecordDefaultValues from '@salesforce/apex/createDocumentDetailAlyanteController.getRecordDefaultValues';
 import { refreshApex } from '@salesforce/apex';
 
-const fields = ['Content__c',
-                'RevenueType__c', 'Document__c',
-                'StartCompetence__c', 'Amount__c',
-                'EndCompetence__c', 'Project__c',
-                'Tags__c'];
+// const fields = ['Content__c',
+//                 'RevenueType__c', 'Document__c',
+//                 'StartCompetence__c', 'Amount__c',
+//                 'EndCompetence__c', 'Project__c',
+//                 'Tags__c'];
+
+const DEBOUNCE_INTERVAL = 300;
+const OBJ_NAME = 'DocumentDetailAlyante__c';
+
 export default class NavToNewRecordWithDefaults extends NavigationMixin(LightningElement) {
+    @api recordId;      //TODO - rename in documentId
+    @api recordTypeId;  //FIXME - not working
+    objectApiName = OBJ_NAME;
+
+    layout = {};
+
+    //TODO - 
+    label = {
+        // cancel: 'Cancel',
+        header: '',
+        new: 'New',
+        // save: 'Save'
+    };
+
     fieldValues = [];
-    @api recordId;
+    // @api recordId;
     projectPopulated = false;
     projectValue;
     @track fetchedData;
-    @wire(getRecordDefaultValues, {documentRecordId: '$recordId'})
+    @wire(getRecordDefaultValues, {documentRecordId: '$recordId'})      //NOTE - rework/improve
     valuesFetched({error, data}) {
         if(data) {
             this.fetchedData = data;
@@ -33,56 +51,115 @@ export default class NavToNewRecordWithDefaults extends NavigationMixin(Lightnin
             }
         }
         if(error) {
-            console.log('ERROR -> ', error)
+            console.error(error);
         }
-        this.formatElementValues();
+        this.projectPopulated = true;
+        // this.formatElementValues();
     }
+
+    renderedCallback() {
+		console.debug('renderedCallback()');
+        const fields = this.template.querySelectorAll('lightning-input-field');
+        if (fields) {
+            fields.forEach(field => {
+                switch (field.fieldName) {
+                    case 'RecordTypeId':
+                        field.value = this.recordTypeId;
+                        break;
+                    case 'Document__c':
+                        field.value = this.recordId;
+                        break;
+                    case 'Project__c':
+                        field.value = this.projectValue;
+                        break;
+                    default:
+                        field.value = null;
+                        break;
+                }
+                console.debug('[default assignment]', field.fieldName, '=>', field.value);
+            });
+        }
+	}
+
+    handleLoad(evt) {
+		console.debug('handleLoad() => ',JSON.parse(JSON.stringify(evt.detail)));
+		// this.layout = this.modifyLayout(this.getEvtLayout(evt));
+        this.layout = this.formatLayout(evt.detail.layout);
+
+		this.label.header = `${this.label.new} ${evt.detail.objectInfos[OBJ_NAME].label}: ${evt.detail.record.recordTypeInfo?.name}`;
+	}
+
+    handleInputChange(evt) {
+		console.debug('handleInputChange() => ',JSON.parse(JSON.stringify(evt.detail)));
+		try {
+			const target = evt.target;
+			this.debounce(function() {
+				const fieldApiName = target.dataset.id;
+				var value = target.value;
+				console.debug('[change]', fieldApiName, '=>', value);
+
+				//TODO - quantity not < 1
+                //FIXME - Tags not deselect all
+
+			}, DEBOUNCE_INTERVAL);
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
+    //TODO - handleSubmit
+
     handleSuccess(event) {
         var detailRecordId = event.detail.id;
         this.dispatchEvent(new ShowToastEvent({
-            title: 'Record Created',
+            title: 'Record Created',        //TODO - custom label
             variant: 'success',
-            message: 'The Record was successfully created',
+            message: 'The Record was successfully created'      //TODO - custom label
         }));
         this.navigateToRecordViewPage(this.recordId);
     }
+
     handleError(event) {
-        // console.log('ERROR -> ', event);
+        //TODO - handle
     }
-    // connectedCallback() {
-    //    console.log('connsected');
-    //    this.formatElementValues();
+
+    timerId;
+	debounce(fn, wait) {
+		clearTimeout(this.timerId);
+		this.timerId = setTimeout(fn, wait);
+	}
+
+    // formatElementValues() {
+    //     this.fieldValues = fields.map( elm => {
+    //         var value = '';
+    //         var size = 6;
+    //         switch (elm) {
+    //             case 'Content__c':
+    //                 size = 12;
+    //                 break;
+    //             case 'Document__c':
+    //                 value = this.recordId;
+    //                 break;
+    //             case 'Project__c':
+    //                 if(this.projectValue) {
+    //                     value = this.projectValue;
+    //                 }
+    //                 break;
+    //             case 'Tags__c':
+    //                 size = 12;
+    //                 break;
+    //             default:
+    //                 break;
+    //         }
+    //         return ({
+    //             name: elm,
+    //             value: value,
+    //             size: size
+    //         })
+    //     })
+    //     this.projectPopulated = true;
     // }
-    formatElementValues() {
-        this.fieldValues = fields.map( elm => {
-            var value = '';
-            var size = 6;
-            switch (elm) {
-                case 'Content__c':
-                    size = 12;
-                    break;
-                case 'Document__c':
-                    value = this.recordId;
-                    break;
-                case 'Project__c':
-                    if(this.projectValue) {
-                        value = this.projectValue;
-                    }
-                    break;
-                case 'Tags__c':
-                    size = 12;
-                    break;
-                default:
-                    break;
-            }
-            return ({
-                name: elm,
-                value: value,
-                size: size
-            })
-        })
-        this.projectPopulated = true;
-    }
+
     navigateToRecordViewPage(recordId) {
         this[NavigationMixin.Navigate]({
             type: 'standard__recordPage',
@@ -93,13 +170,14 @@ export default class NavToNewRecordWithDefaults extends NavigationMixin(Lightnin
         })
         this.resetFieldsAndRefresh();
     }
-    resetFieldsAndRefresh() {
+
+    resetFieldsAndRefresh() {       //FIXME - not working
         const inputFields = this.template.querySelectorAll(
             'lightning-input-field'
         );
         if (inputFields) {
             inputFields.forEach(field => {
-                console.log(field.value);
+                console.log(field.value);       //TODO - rework c log
                 if(field.value != this.recordId && field.value != this.projectValue) {
                     field.reset();
                 }
@@ -109,7 +187,43 @@ export default class NavToNewRecordWithDefaults extends NavigationMixin(Lightnin
         // this.projectPopulated = false;
         // refreshApex(this.fetchedData);
     }
+
     goBack() {
         this.navigateToRecordViewPage(this.recordId);
     }
+
+    formatLayout(layout) {
+		const isModeView = layout.mode == "View";
+		const isModeCreate = layout.mode == "Create";
+		const sections = layout.sections.map( section => {
+			const sectionColumns = section.columns || 1;
+			const layoutRows = section.layoutRows.map(layoutRow => {
+				const layoutItems = layoutRow.layoutItems.map(layoutItem => {
+					var size = Math.round(12/sectionColumns);
+					var editable = isModeView ? false : (isModeCreate ? layoutItem.editableForNew : layoutItem.editableForUpdate);
+					return {
+						...layoutItem,
+						size: size,
+						editable: editable
+					};
+				});
+				return {
+					...layoutRow,
+					layoutItems: layoutItems
+				};
+			});
+			return {
+				...section,
+				layoutRows: layoutRows
+			};
+		});
+        var sectionsWithoutSystemInfo = sections.filter(item => item.heading != 'System Information');
+		layout = {
+			...layout,
+			sections: sectionsWithoutSystemInfo
+		};
+
+		console.log('Layout => ',JSON.parse(JSON.stringify(layout)));
+		return layout;
+	}
 }
